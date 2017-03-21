ad4a8094721e37eb3d406f14aa659970311ed9df
SYH <>
2017-03-21 18:36:38 +0900

    chessGame주석처리

--- ChessGamePlay.py
+++ ChessGamePlay.py
@@ -1,1 +1,28 @@
 #체스게임 플레이
+
+import chess
+import MontecarloTreeSearch as Monte
+
+board = chess.Board()
+
+def startBoard():
+    board = chess.Board()
+
+def push(str):
+    board.push_san(str)
+
+startBoard()
+
+while(True):
+    print(board)
+
+    #print(board.turn) # true 는 white false 는 black
+    if (board.turn):
+        choice = input("white choice ( exit = 0 ) : ")
+    else :
+        choice = input("black choice ( exit = 0 ) : ")
+
+    if (choice == 0) :
+        break
+    else :
+        board.push_san(choice)
