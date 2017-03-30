46c6457b796366345f8e858afa84a5d905318202
multimedia <seoyh0734@naver.com>
2017-03-30 20:10:07 +0900

    Node.py에 필요한 변수 업데이트
    Tree.py에 가능한 경우의 수로 트리만드는 함수 make_nodes() 수정
    
    쓰레기코드임

--- Test.py
+++ Test.py
@@ -1,9 +1,42 @@
 import chess
-import sys
+from GetBoardString import get_BoardString as GB
+from MakeLegalMoves import make
+import Tree
 
-tmp=chess.Board()
+tmp = chess.Board()
 
-tmp
+tmpStr = GB(tmp)
 
-#print(sys.getsizeof(chess.Board), sys.getsizeof(tmp))
+tree = Tree.Tree()
 
+root = tree.play_tree(tmpStr)
+
+# #
+# import chess
+# from GetBoardString import get_BoardString as GB
+# from MakeLegalMoves import make
+#
+# tmp=chess.Board()
+#
+# tmpstr = GB(tmp)
+#
+# print(tmpstr)
+#
+# print("AA")
+#
+# board2 = chess.Board(tmpstr)
+#
+# board2.push_san("c4")
+#
+# legal = board2.legal_moves
+#
+# legal = str(legal)
+#
+# print(type(legal))
+# print(legal)
+#
+# legal = make(legal)
+#
+# print(legal)
+
+#print(MLM.make(.legal_moves)))
