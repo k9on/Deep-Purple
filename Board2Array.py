9db51da67855ea285a157b1c36c6e25d02408cc7
k9on <keon9@naver.com>
2017-04-06 20:03:37 +0900

    Update Board2Array.py

--- Board2Array.py
+++ Board2Array.py
@@ -11,31 +11,18 @@ class Board2Array:
             boardArray.append(-1)
 
         for i in range(len(str)):
-            if str[i] == 'K':
-                boardArray[i] = 1
-            elif str[i] == 'Q':
-                boardArray[i] = 2
-            elif str[i] == 'N':
-                boardArray[i] = 3
-            elif str[i] == 'B':
-                boardArray[i] = 4
-            elif str[i] == 'P':
-                boardArray[i] = 5
-            elif str[i] == 'R':
-                boardArray[i] = 6
-            elif str[i] == 'k':
-                boardArray[i] = 7
-            elif str[i] == 'q':
-                boardArray[i] = 8
-            elif str[i] == 'n':
-                boardArray[i] = 9
-            elif str[i] == 'b':
-                boardArray[i] = 10
-            elif str[i] == 'p':
-                boardArray[i] = 11
-            elif str[i] == 'r':
-                boardArray[i] = 12
-            elif str[i] == '.':
-                boardArray[i] = 0
+            if str[i] == 'K': boardArray[i] = 1
+            elif str[i] == 'Q': boardArray[i] = 2
+            elif str[i] == 'N': boardArray[i] = 3
+            elif str[i] == 'B': boardArray[i] = 4
+            elif str[i] == 'P': boardArray[i] = 5
+            elif str[i] == 'R': boardArray[i] = 6
+            elif str[i] == 'k': boardArray[i] = 7
+            elif str[i] == 'q': boardArray[i] = 8
+            elif str[i] == 'n': boardArray[i] = 9
+            elif str[i] == 'b': boardArray[i] = 10
+            elif str[i] == 'p': boardArray[i] = 11
+            elif str[i] == 'r': boardArray[i] = 12
+            elif str[i] == '.': boardArray[i] = 0
 
         return boardArray
