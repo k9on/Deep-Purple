cae9b23dbcdc084d2038965b4881d9914b2ab949
k9on <keon9@naver.com>
2017-04-06 18:26:11 +0900

    Update MontecarloTreeSearch.py

--- MontecarloTreeSearch.py
+++ MontecarloTreeSearch.py
@@ -9,22 +9,53 @@ MCTS(Monte Calro Tree Search)
 4. 역전파: 확장된 노드의 가치를 역전파 하여 승산 가능성을 update
 
 '''
-
+import Tree as TR
+import MakeLegalMoves as MLM
+import chess
 class Monte:
     def __init__(self):
-        self.tree
 
-    def asking(self, board):
-        solution = str() # 가장 좋은수
-        root = self.tree.Play_Tree(board) # 트리한테 결과트리의 루트를 반환
-        children = root.get_AllChild() # 루트로 부터 모든 legal child를 반환
-        if(len(children)==0):
-            print("가능한 수가 없음")
-            solution = "0"
-        else:
-            solution = self.decide_BestMove(children)
-        return solution
+        self.tree = None
+        self.inputBoardString =None #처음에 입력 받은 게임의 체스보드 상태
 
-    def decide_BestMove(self, children):
-        '''가장 좋은 수 추출'''
-        return children[0].command
+
+    def set_Board(self, board):
+        #몬테카를로 트리탐색 시작 전 현재 체스 보드 저장
+        self.present_board= board.copy()
+
+    def set_inputBoardString(self,str):
+        #기보 형식으로 inputBoardString을 string으로 입력 받음
+        self.inputBoardString = str.copy()
+
+    def monteCarloTreeSearch(self,boardString):
+
+        self.tree = TR.Tree(boardString) #Tree 객체 생성  #Tree의 루트 노드생성
+
+        simulation_Number = 4 #임의의 시뮬레이션 숫자
+        for i in range(simulation_Number): #수행할 시뮬레이션 횟수
+            #print(self.tree.board_stack.display_Board())
+
+            print("%s  " %(i), end='',flush=True )
+            num=0
+            while self.tree.get_GameOver() == False: #게임이 종료될때까지
+            #tree의 currentNode가 종료 되었는지
+
+                #다음 노드를 선택하는 확장 진행
+                tempNode = self.tree.make_MonteCarloNextChild()
+                self.tree.add_ChildNode(tempNode) #임의로 생성된 노드를 트리의 자식에 붙인다
+                self.tree.set_CurrentNode(tempNode) # 자식이 생성 되었으므로 자식을 currentNode로 설정
+                num +=1
+                 #턴 넘김 swap player 자식 노드를 만들때 Push가 되어 턴이 바뀌게 되었다.
+            print("몬테카를로 Depth = ",num, end='',flush=True)
+            #while문을 끝나고 나왔을때 self.tree의 currentNode는 마지막 노드를 가지므로
+            #currentNode부터 backpropagation을 진행하면 된다.
+            self.tree.set_GameResult()
+            #역전파 backbpropagation
+            self.tree.backpropagation()
+
+        #가장 좋은 수를 반환
+        return self.tree.get_BestMove()
+
+    #def get_BestMove(self):
+
+
